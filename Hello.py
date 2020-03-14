4067e5d0d501c4e8d290defd5c81c1ffecf1ecc1
ryo589 <ryo.xxx.abema@gmail.com>
2020-03-14 19:07:10 +0900

    hello

---
+++ Hello.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return '<html><body><h1>sample by Abema System</h1></body></html>'
+if __name__ == '__main__':
+    app.run()
