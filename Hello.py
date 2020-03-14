96f68c5b31253abf11d11c784ca7c63d62508c1c
ryo589 <ryo.xxx.abema@gmail.com>
2020-03-14 21:35:20 +0900

    ryo

--- Hello.py
+++ Hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return '<html><body><h1>sample by Abema System</h1></body></html>'
+    return '<html><body><h1>sample by Abema System　ryo</h1></body></html>'
 if __name__ == '__main__':
     app.run()
